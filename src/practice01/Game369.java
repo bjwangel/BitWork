247bc4933f7877748ca8f32bb8b60a1d9de8142c
변준우 <bjwangel@naver.com>
2016-09-06 17:40:04 +0900

    1st

--- src/practice01/Game369.java
+++ src/practice01/Game369.java
@@ -1,15 +1,31 @@
 package practice01;
+
 public class Game369 {
 	public static void main(String[] args) {
-		
-		int number=123123;
-		String s=String.valueOf(number); //valueof(int)
-		
-		System.out.println(s.length());
-		
-		char c=s.charAt(2);
-		System.out.println(c);
-		
-//		if()
+
+		int num = 1;
+
+		for (num = 1; num < 100; num++) {
+			String s = String.valueOf(num);
+			if (num < 10) {
+				char c1 = s.charAt(0);
+				if (c1 == 3 || c1 == 6 || c1 == 9) {
+					System.out.println(num + "짝!!");
+				}
+			}
+			if (num >= 10 && num < 100) {
+				char c2 = s.charAt(0);
+				char c3 = s.charAt(1);
+				if (c2 == 3 || c2 == 6 || c2 == 9) {
+					System.out.print(num + "짝!!"); // 줄 바꾸지 않음
+				}
+				if (c3 == 3 || c3 == 6 || c3 == 9) {
+					System.out.println("짝!!"); // 줄 바꿈
+				} else {
+					System.out.println("");
+				}
+			}
+		}
+
 	}
 }
--- src/practice01/MultipleOfTree.java
+++ src/practice01/MultipleOfTree.java
@@ -6,6 +6,14 @@ public class MultipleOfTree {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner( System.in );
+		System.out.println("판별할 수를 입력하세요");
+		int num=scanner.nextInt();
 		scanner.close();
+		
+		if(num%3==0){
+			System.out.println(num+"은 3의 배수 입니다.");
+		}else{
+			System.out.println(num+"은 3의 배수가 아닙니다.");
+		}
 	}
 }
